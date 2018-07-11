88299b17774a304edc7c630f80b46ae267e096a3
eholzbach <ericholzbach@gmail.com>
2018-07-10 21:29:23 -0700

    dont map messages without coordinates

--- server.go
+++ server.go
@@ -26,9 +26,11 @@ func renderPage(db0 *redis.Client) http.Handler {
 		var data []string
 		for _, v := range a {
 			b, _ := db0.HMGet(v, "longitude", "latitude", "device", "country").Result()
-			icon := getType(b[2])
-			c := fmt.Sprintf("L.marker([%s,%s], {icon: %s}, {title: '%s'}).addTo(mymap).bindPopup('%s<br><br>mmsi: %s<br>country: %s').on('mouseover', function (e) { this.openPopup()}).on('mouseout', function (e) { this.closePopup()}).on('click', function (e) { window.open(\"https://www.marinetraffic.com/en/ais/details/ships/mmsi:%s\")});", b[1], b[0], icon, v, b[2], v, b[3], v)
-			data = append(data, c)
+			if b[0] != nil && b[1] != nil {
+				icon := getType(b[2])
+				c := fmt.Sprintf("L.marker([%s,%s], {icon: %s}, {title: '%s'}).addTo(mymap).bindPopup('%s<br><br>mmsi: %s<br>country: %s').on('mouseover', function (e) { this.openPopup()}).on('mouseout', function (e) { this.closePopup()}).on('click', function (e) { window.open(\"https://www.marinetraffic.com/en/ais/details/ships/mmsi:%s\")});", b[1], b[0], icon, v, b[2], v, b[3], v)
+				data = append(data, c)
+			}
 		}
 		t, _ := template.ParseFiles("index.html")
 		t.Execute(w, data)
